ee0cd53f2663f21a1a1d47ce7587d503730253f4
dheanaura <oktavianidhea4@gmail.com>
2023-12-02 18:45:02 +0700

    Initial commit

---
+++ ecommerce/Pages/index.vue
@@ -0,0 +1,20 @@
+<!-- pages/index.vue -->
+<template>
+  <div class="home">
+    <h1 class="text-5xl font-bold">
+Selamat Datang di Pusat Elektronik Terpercaya!</h1>
+    <p class="text-lg mt-4">Temukan kenyamanan dan kemudahan hidup Anda dengan koleksi perangkat elektronik terbaik di toko kami. Dari hiburan hingga peralatan rumah tangga, kami menyediakan beragam produk elektronik terbaik dengan kualitas terjamin dan harga yang kompetitif.</p>
+  </div>
+</template>
+
+<style scoped>
+/* Add your custom styles here */
+.home {
+  background-color: #3498db; /* Choose your desired background color */
+  color: #fff; /* Set text color to contrast with the background */
+  padding: 150px; /* Add padding as needed */
+  text-align: center; /* Center the content */
+}
+
+/* Add more styles as needed */
+</style>
---
+++ ecommerce/composables/constants/category.ts
@@ -0,0 +1,30 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Magic com",
+    },
+    {
+    id: 2,
+    name: "Mesin cuci",
+    },
+    {
+    id: 3,
+    name: "Kipas angin",
+    },
+    {
+    id: 4,
+    name: "Oven",
+    },
+    {
+    id: 5,
+    name: "Tv",
+    },
+    {
+        id: 6,
+        name: "Setrika",
+        },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
